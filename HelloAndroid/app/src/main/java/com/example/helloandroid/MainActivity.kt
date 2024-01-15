cb6e47f9c2cdd672d37e87ca17de5149a0a50aa1
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-15 09:47:25 +0900

    再作成

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.helloandroid
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloandroid.ui.theme.HelloAndroidTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
