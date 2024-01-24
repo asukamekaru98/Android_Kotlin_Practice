f7ebea3412ea759ec59e3b9b6d03cdbe4e938230
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-24 23:09:04 +0900

    4.3

--- HelloSample/app/src/main/java/com/webserva/wings/android/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/com/webserva/wings/android/hellosample/MainActivity.kt
@@ -16,15 +16,29 @@ class MainActivity : AppCompatActivity() {
         val btClick = findViewById<Button>(R.id.btClick)
         val listener = HelloListener()
         btClick.setOnClickListener(listener)
+
+        val btClear = findViewById<Button>(R.id.btClear)
+        btClear.setOnClickListener(listener)
     }
 
     private inner class HelloListener: View.OnClickListener {
         override fun onClick(view: View){
 
-            val input = findViewById<EditText>(R.id.etName)
-            val output= findViewById<TextView>(R.id.tvOutput)
-            val inputStr = input.text.toString()
-            output.text = inputStr + "さん、こんにちは"
+            val input = findViewById<EditText>(R.id.etName)//入力欄
+            val output= findViewById<TextView>(R.id.tvOutput)//メッセージ
+
+            when(view.id){
+                R.id.btClick->{
+                    val inputStr = input.text.toString()
+                    output.text = inputStr + "さん、こんにちは"
+                }
+
+                R.id.btClear->{
+                    input.setText("")
+                    output.text = ""
+                }
+            }
+
         }
     }
 }
