c9ada8893d8eae6c2a32a9fca6b90e9e6f8f0dc4
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-25 11:22:11 +0900

    5.3 *memo1* Androidダイアログは、Positive,Negative,Neutralの3つ 多分純正品？ 位置も勝手に決めてそう。3つ揃ってなくてもOK、純正で決めている位置のボタンが非表示になるだけみたい
    **
    
    *memo2*
    セーフコール演算子 = 変数のあとにつく"?"のこと
    この演算子は、変数がnullでなければメソッドにアクセスし、nullならnullを返すという意味になります。
    
    *memo3*
    メソッドとは
    例えば、あるオブジェクトが「車」である場合、その「メソッド」は「走る」「止まる」「ハンドルを切る」など、
    車が行うことができる操作を定義したものになります。
    
    *memo4*
    Kotlin言語の「nullable」は、変数がnullを許容するかどうかを示す概念
    Kotlinでは、基本的に全ての型は非null型として定義され、nullを代入することはできないが、
    場合によっては変数を「何もない」という状態（null）にしておいた方が便利な時もあり、そのような時に
    nullを変数に代入できるようにすることを「null許容型（Nullable）」と言う。
    ?をつければ許容、つけなければ非許容

--- ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/MainActivity.kt
+++ ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/MainActivity.kt
@@ -2,8 +2,11 @@ package com.websarva.wings.android.listviewsample2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +24,15 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ArrayAdapter(this@MainActivity,android.R.layout.simple_list_item_1,menuList)
         lvMenu.adapter = adapter
+
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener:AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val dialogFragment = OrderConfirmDialogFragment()
+
+            dialogFragment.show(supportFragmentManager,"OrderConfirmDialogFragment")
+        }
     }
 }
