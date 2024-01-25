c665d204e9de81f41a94378d2e2cc914d9e56477
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-25 14:30:41 +0900

    7.2 *メモ* AndroidStudioの画面遷移機能は 実際には画面の上に画面を重ねてる状態なんだって。

--- IntentSample/app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.websarva.wings.android.intentsample
 
+import android.app.LauncherActivity.ListItem
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import android.widget.TextView
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +35,25 @@ class MainActivity : AppCompatActivity() {
         val to = intArrayOf(android.R.id.text1,android.R.id.text2)
         val adapter = SimpleAdapter(this@MainActivity,menuList,android.R.layout.simple_list_item_2,from,to)
         lvMenu.adapter = adapter
+
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent:AdapterView<*>, view: View, position: Int, id: Long){
+            val item = parent.getItemAtPosition(position) as MutableMap<String,String>
+
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+
+            val intent2MenuThanks = Intent(this@MainActivity,MenuThanksActivity::class.java)
+
+            intent2MenuThanks.putExtra("menuName",menuName)
+            intent2MenuThanks.putExtra("menuPrice",menuPrice)
+
+            startActivity(intent2MenuThanks)
+
+        }
     }
 
 
