efe64f1b3f6e588edbab62f11deee824ef0a5225
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-26 11:08:29 +0900

    9.3 *メモ* activityプロパティはNullable型なので そのintentプロパティを利用するとき、セーフコール演算子を記述する必要がある

--- FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MainActivity.kt
+++ FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.websarva.wings.android.fragmentsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // lvMenu.onItemClickListener = ListItemClickListener()
     }
+
+   /* private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent:AdapterView<*>, view: View, position: Int, id: Long){
+            val item = parent.getItemAtPosition(position) as MutableMap<String,String>
+
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+
+            val intent2MenuThanks = Intent(activityResultRegistry,  MenuThanksActivity::class.java)
+
+            intent2MenuThanks.putExtra("menuName",menuName)
+            intent2MenuThanks.putExtra("menuPrice",menuPrice)
+
+            startActivity(intent2MenuThanks)
+
+        }
+    }*/
 }
--- FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuListFragment.kt
+++ FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuListFragment.kt
@@ -1,15 +1,18 @@
 package com.websarva.wings.android.fragmentsample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import android.widget.TextView
 
 class MenuListFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_menu_list, container, false)
@@ -32,8 +35,29 @@ class MenuListFragment : Fragment() {
         val adapter = SimpleAdapter(activity,menuList,android.R.layout.simple_list_item_2,from, to)
         lvMenu.adapter = adapter
 
+        lvMenu.onItemClickListener = ListItemClickListener()
+
         return view
     }
 
-
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            // タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMutableMap型!
+            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+            // 定食名と金額を取得。
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+            // インテントオブジェクトを生成。
+            val intent2MenuThanks = Intent(activity, MenuThanksActivity::class.java)
+            // 第2画面に送るデータを格納。
+            intent2MenuThanks.putExtra("menuName", menuName)
+            intent2MenuThanks.putExtra("menuPrice", menuPrice)
+//			val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java).apply {
+//				putExtra("menuName", menuName)
+//				putExtra("menuPrice", menuPrice)
+//			}
+            // 第2画面の起動。
+            startActivity(intent2MenuThanks)
+        }
+    }
 }
