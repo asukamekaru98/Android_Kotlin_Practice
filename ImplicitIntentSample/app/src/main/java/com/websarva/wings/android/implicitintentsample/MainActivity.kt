f29cf204ceabef3d1466124a0dae2c839ddd9149
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-29 15:53:34 +0900

    14.3 *memo* 位置情報を使用する時、FusedLocationProviderClientを使う。 またLocationRequest()で、位置情報を取得する。 このprioriryプロパティで、位置情報精度を決める。 精度が高ければ消費電力は大きいし、精度が低ければ消費電力は小さい

--- ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
+++ ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
@@ -6,6 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +19,21 @@ class MainActivity : AppCompatActivity() {
     private var _latitude = 0.0
     private var _longitude = 0.0
 
+    private lateinit var _fusedLocationClient: FusedLocationProviderClient
+    private lateinit var _locationRequest: LocationRequest
+    private lateinit var _onUpdateLocation: OnUpdateLocation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        _locationRequest = LocationRequest.create()
+        _locationRequest?.let{
+            it.interval = 5000
+            it.fastestInterval = 1000
+            it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        _onUpdateLocation = OnUpdateLocation()
     }
 
     fun onMapSearchButtonClick(view: View){
@@ -36,5 +54,31 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
+
+    private inner class OnUpdateLocation: LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult?) {
+           locationResult?.let {
+               val location = it.lastLocation
+               location?.let {
+                   _latitude = it.latitude
+                   _longitude = it.longitude
+                   val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
+                   tvLatitude.text = _latitude.toString()
+                   val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
+                   tvLongitude.text = _longitude.toString()
+               }
+           }
+        }
+    }
+    override fun onResume(){
+        super.onResume()
+        _fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation, mainLooper)
+    }
+
+    override fun onPause(){
+        super.onPause()
+        _fusedLocationClient.removeLocationUpdates(_onUpdateLocation)
+    }
+
 
 }
