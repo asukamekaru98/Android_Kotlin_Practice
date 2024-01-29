4d2b400cb9c5b9f99301300dd3d7cdbcd3ff1bae
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-29 17:09:39 +0900

    15.2 *memo* アンドロイドストレージ内のファイルは、基本的にURIで指定する。 1.画像ファイル名を生成 2.Uriオブジェクト生成に必要なContentValueオブジェクトを生成 3.URIのオブジェクトを生成 という流れ

--- CameraIntentSample/app/src/main/java/com/websarva/wings/android/cameraintentsample/MainActivity.kt
+++ CameraIntentSample/app/src/main/java/com/websarva/wings/android/cameraintentsample/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.websarva.wings.android.cameraintentsample
 
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    private var _imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,14 +25,30 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode==200 && resultCode== RESULT_OK){
-            val bitmap = data?.getParcelableExtra<Bitmap>("data")
+            //val bitmap = data?.getParcelableExtra<Bitmap>("data")
+            //val ivCamera = findViewById<ImageView>(R.id.ivCamera)
+            //ivCamera.setImageBitmap(bitmap)
             val ivCamera = findViewById<ImageView>(R.id.ivCamera)
-            ivCamera.setImageBitmap(bitmap)
+            ivCamera.setImageURI(_imageUri)
         }
     }
 
+
     fun onCameraImageClick(view: View){
+
+        val dateFormat = SimpleDateFormat("yyyyMMddHHss")
+        val now = Date()
+        val nowStr = dateFormat.format(now)
+        val fileName = "CameraIntentSamplePhoto_${nowStr}.jpg"
+
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE,fileName)
+        values.put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg")
+
+        _imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,_imageUri)
         startActivityForResult(intent, 200)
 
     }
