82cef830e5053ff703f770bb32e4619758c8eb8a
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-29 13:33:42 +0900

    13.2

--- ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/SoundManageSevice.kt
+++ ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/SoundManageSevice.kt
@@ -1,5 +1,8 @@
 package com.websarva.wings.android.servicesample
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
@@ -7,14 +10,30 @@ import android.net.Uri
 import android.os.IBinder
 import android.provider.MediaStore.Audio.Media
 import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class SoundManageSevice : Service() {
+
+    companion object {
+        private const val CHANNEL_ID = "soundmanagerservice_notification_channel"
+    }
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
     private var _player: MediaPlayer? = null
+
+    override fun onCreate() {
+        _player = MediaPlayer()
+        val name = getString(R.string.notification_channel_name)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance)
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val mediaFileUriStr = "android.resource://${packageName}/${R.raw.saya}"
 
@@ -45,8 +64,15 @@ class SoundManageSevice : Service() {
         }
     }
 
-    private inner class PlayerCompletionListener:MediaPlayer.OnCompletionListener{
+    private inner class PlayerCompletionListener : MediaPlayer.OnCompletionListener {
         override fun onCompletion(mp: MediaPlayer) {
+            val builder = NotificationCompat.Builder(this@SoundManageSevice, CHANNEL_ID)
+            builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+            builder.setContentTitle(getString(R.string.msg_notification_title_finish))
+            builder.setContentText(getString(R.string.msg_notification_text_finish))
+            val notification = builder.build()
+            val manager = NotificationManagerCompat.from(this@SoundManageSevice)
+            manager.notify(100, notification)
             stopSelf()
         }
     }
