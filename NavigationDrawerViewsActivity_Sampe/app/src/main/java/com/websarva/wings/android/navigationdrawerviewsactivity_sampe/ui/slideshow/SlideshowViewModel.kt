1c032724d41152ca6d54e09d8ea5d5dc0cd2ef3e
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-01-30 17:38:38 +0900

    samples

---
+++ NavigationDrawerViewsActivity_Sampe/app/src/main/java/com/websarva/wings/android/navigationdrawerviewsactivity_sampe/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.navigationdrawerviewsactivity_sampe.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
